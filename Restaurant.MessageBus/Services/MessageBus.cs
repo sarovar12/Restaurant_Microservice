7051b939c4b3359a876563bdfeed7acaeb1eff60
sarovar12 <85166131+sarovar12@users.noreply.github.com>
2024-02-19 22:02:16 +0545

    Test: Set up gitignore to hide connection string of azure service bus

---
+++ Restaurant.MessageBus/Services/MessageBus.cs
@@ -0,0 +1,15 @@
+﻿
+
+using Azure.Messaging.ServiceBus;
+using Restaurant.MessageBus.Interfaces;
+
+namespace Restaurant.MessageBus.Services
+{
+    public class MessageBus : IMessageBus
+    {
+        public async Task PublishMessage(object message, string topic_queue_Name)
+        {
+            await using var client = new ServiceBusClient();
+        }
+    }
+}
